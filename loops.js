a07260e0e9ef3dfe1543f62dd5c4b7f7b90df5eb
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-10-27 19:57:05 -0400

    Second Commit
    complete up to question 4 of loops.js

---
+++ loops.js
@@ -0,0 +1,19 @@
+// 1. Create a loop that logs the numbers from 0-99 (ascending)
+for (let k = 0 ; k < 100 ; k++) {
+    // console.log(k); 
+}
+
+// 2. Create a loop that logs the numbers from 99-0 (descending)
+for (let k = 99 ; k >= 0 ; k--) {
+    // console.log(k); 
+}
+
+// 3. Create a loop that logs the EVEN numbers from 0-98 (ascending)
+for (let k = 0; k < 99; k += 2 ) {
+    // console.log(k); 
+}
+
+// 4. Create a loop that logs the EVEN numbers from 98-0 (descending)
+for (let k = 98 ; k > 0 ; k -= 2) {
+    console.log(k); 
+}
